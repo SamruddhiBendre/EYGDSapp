3836f15e0376c0c971185c3ce08aad2b69fa46b5
SamruddhiBendre <118345269+SamruddhiBendre@users.noreply.github.com>
2025-03-07 13:44:12 +0530

    Update auth.js

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -24,7 +24,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const result = await axios.post("https://eygdsapp.onrender.com", {
+      const result = await axios.post("https://eygdsapp.onrender.com/auth/login", {
         username,
         password,
       });
@@ -75,7 +75,7 @@ const Register = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("https://eygdsapp.onrender.com", {
+      await axios.post("https://eygdsapp.onrender.com/auth/register", {
         username,
         password,
       });
@@ -112,3 +112,4 @@ const Register = () => {
     </div>
   );
 };
+
